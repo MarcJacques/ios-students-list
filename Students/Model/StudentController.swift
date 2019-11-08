49345f014e5a9785a2e59b68ba70b3e976eaeb79
Marc Jacques <Marcjacques@me.com>
2019-11-08 09:48:05 -0500

    Created a load function on modelController and setup tableView dataSource using the data we load from persistent store.

--- Students/Model/StudentController.swift
+++ Students/Model/StudentController.swift
@@ -14,4 +14,36 @@ class StudentController {
         guard let filePath = Bundle.main.path(forResource: "students", ofType: "json") else { return nil }
         return URL(fileURLWithPath: filePath)
     }
+    
+    func loadFromPersistentStore(completion: @escaping ([Student]?, Error?) -> Void)
+        //this is a closure, an anonymous function, the only argument it accepts is one called a completion, the type is everythinig from "@escaping to Error?", this closure does not return anything. Closures are required to specify a return this is an example of it returning nothing.
+    {
+        //run this process in the background queue
+        let bgQueue = DispatchQueue(label: "studentQueue", attributes: .concurrent)
+        bgQueue.async {// we want this to run on the background.
+            // were trying to load the data from the file
+            let fm = FileManager.default
+            guard let url = self.persistentFileURL,
+                fm.fileExists(atPath: url.path) else { return }// does this file exist? if not jump out early
+            
+            // open the file, read it, decode it
+            do {
+                let data = try Data(contentsOf: url) // find this data in url and try to store it into data variable
+                // make it useful to us by decoding it
+                let decoder = JSONDecoder()
+                let students = try decoder.decode([Student].self, from: data) //use the data model in a way that matches the format of the JSON and the data that you just created. were telling swift to compare the model to the actual JSON so we can properly decode it. our data model must conform to the JSON to work
+                //now we need a way to send the data to the VC. right now it only exists in this do block
+                completion(students, nil)
+                //if the do block succeeds we don't need to catch anything. ***** However if it fails to load the data on line 33 or fails to read or decode the data, the compiler will not run line 35. We want it to catch the error so we know why we were unable to get our student data********
+            } catch {
+                //if either of the steps above in the do block throw an error we catch it by doing the following so we know what happened
+                print("Error loading student data: \(error)")
+                //if we catch an error students will be nil and we want to pass the error object to our completion
+                completion(nil, error)
+            }
+        }
+    }
+    func filter(with trackType: TrackType, sortedBy sorter: SortOptions, completion: @escaping ([Student]) -> Void) {
+        
+    }
 }
--- Students/View Controllers/StudentsViewController.swift
+++ Students/View Controllers/StudentsViewController.swift
@@ -16,13 +16,32 @@ class StudentsViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     // MARK: - Properties
+    private let studentController = StudentController()
+    
+    private var filteredAndSortedStudents: [Student] = [] {
+        didSet {
+            tableView.reloadData() // automatically update once the data changes
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         //defining the tableview datasource
         tableView.dataSource = self
-
+        // call the function that loads the data at the start of the app before anything.
+        studentController.loadFromPersistentStore(completion: { students, error in
+            // its called completion because this is work we want it to run when the work is finished
+            // 1st lets findout if there was any problem or error while loading the data
+            if let error = error {
+                print("Error loading students: \(error)")
+            }
+            //now if it doesn't catch an error we want it to if the data exists unwrap it, store it in this constant, then store it in the tableView
+            DispatchQueue.main.async { // All UI updates must be performed on the main queue. if your on another queue run this on the main queue asyncrhonously
+                if let students = students {
+                    self.filteredAndSortedStudents = students // now the data we pulled form the JSOn is availalbe to the res of this view to use and load into the tableview.
+                }
+            }
+        })
     }
     
     // MARK: - Action Handlers
@@ -38,13 +57,17 @@ class StudentsViewController: UIViewController {
 
 extension StudentsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return filteredAndSortedStudents.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "StudentCell", for: indexPath)
         
         // Configure cell
+        
+        let aStudent = filteredAndSortedStudents[indexPath.row]
+        cell.textLabel?.text = aStudent.name
+        cell.detailTextLabel?.text = aStudent.course
         
         return cell
     }
